ae35c286e1cca9a07d043fad7c9909e47aac99ba
Darren Hale <darrenhale@darrenhale.clearent.lan>
2015-11-20 10:31:05 -0600

    Set up a basic Xunit test and command.

---
+++ Runner_UT.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XSqlClient.Tests
+{
+  public class RunnerTests
+  {
+
+    public RunnerTests()
+    {
+    }
+
+    [Fact]
+    public async Task CanParseArguments()
+    {
+      Assert.True(false);
+    }
+  }
+}
